6396a12270aa932d9ab35da0d0499d5b341da477
J. Andreu <jandreu@sofistic.com>
2021-02-21 02:19:55 +0100

    Inicializacion

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+ 
+setup(
+    name='yalp-batch',
+    packages=['yalp-batch'],
+    version='99.99.99',
+    license='LGPL v3', 
+    description='POC',
+    author='xtripulant',
+    author_email='xtripulant.bbounty@gmail.com',
+    url='https://github.com/xtripulant83/yalp-batch', # Usa la URL del repositorio de GitHub
+    download_url='https://github.com/RDCH106/parallel_foreach_submodule/archive/v0.1.tar.gz', # Te lo explico a continuación
+    keywords='test example develop', # Palabras que definan tu paquete
+    classifiers=['Programming Language :: Python',  # Clasificadores de compatibilidad con versiones de Python para tu paquete
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7'],
+)
